52d9dacace56c2d00906c965649033ce19aadb71
philippjan <philipp.stassen@gmail.com>
2018-06-01 18:52:15 +0200

    Vortrag1 module, added Parsing and Printing

---
+++ Parsing_FOL.hs
@@ -0,0 +1,195 @@
+-- Parsing of formulas --
+-- After Harrison "Handbook ... "
+module Parsing_FOL where
+
+import Data.String
+
+-- Chapter 1.7 Lexing --
+space x = x `elem` " \t\n\r"
+
+punctuation x = x `elem` "()[]{},"
+
+symbolic x = x `elem` "~‘!@#$%^&*-+=|\\:;<>.?/"
+
+numeric x = x `elem` "0123456789"
+
+alphanumeric x =
+  x `elem` "abcdefghijklmnopqrstuvwxyz_’ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+lexwhile prop (c:cs)
+  | prop c = (c : tok, rest)
+  where
+    (tok, rest) = lexwhile prop cs
+lexwhile prop x = ("", x)
+
+lexer inp
+  | (snd (lexwhile space inp) == "") = []
+  | otherwise =
+    let c = head (snd (lexwhile space inp))
+        cs = tail (snd (lexwhile space inp))
+        prop
+          | alphanumeric c = alphanumeric
+          | symbolic c = symbolic
+          | otherwise = \x -> False
+        (toktl, rest) = lexwhile prop cs
+     in (c : toktl) : (lexer rest)
+
+-- Chapter 1.7 Parsing --
+make_parser pfn s =
+  let (expr, rest) = pfn (lexer s)
+   in if rest == []
+        then expr
+        else error "Unparsed input"
+
+-- Appendix 3 General parsing functions
+parse_ginfix opsym opupdate sof subparser inp
+  | (inp1 /= [] && head inp1 == opsym) =
+    parse_ginfix opsym opupdate (opupdate sof e1) subparser (tail inp1)
+  | otherwise = (sof e1, inp1)
+  where
+    (e1, inp1) = subparser inp
+
+parse_left_infix opsym opcon =
+  parse_ginfix opsym (\f e1 e2 -> (opcon (f e1) e2)) (\x -> x)
+
+parse_right_infix opsym opcon =
+  parse_ginfix opsym (\f e1 e2 -> f (opcon e1 e2)) (\x -> x)
+
+parse_list opsym = parse_ginfix opsym (\f e1 e2 -> (f e1) ++ [e2]) (\x -> [x])
+
+papply f (ast, rest) = (f ast, rest)
+
+nextin inp tok = (inp /= []) && (head inp == tok)
+
+parse_bracketed subparser cbra inp
+  | nextin rest cbra = (ast, tail rest)
+  | otherwise = error "Closing bracket expected"
+  where
+    (ast, rest) = subparser inp
+
+-- Appendix 3 Parsing first-order terms --
+is_const_name s = (all numeric s) || (s == [])
+
+data Term
+  = Var String
+  | Fn String
+       [Term]
+  deriving (Show, Eq)
+
+parse_atomic_term [] = error "term expected"
+parse_atomic_term ("(":rest) = parse_bracketed parse_term ")" rest
+parse_atomic_term ("-":rest) =
+  papply (\t -> Fn "-" [t]) (parse_atomic_term rest)
+parse_atomic_term (f:"(":")":rest) = (Fn f [], rest)
+parse_atomic_term (f:"(":rest) =
+  papply
+    (\args -> Fn f args)
+    (parse_bracketed (parse_list "," parse_term) ")" rest)
+parse_atomic_term (a:rest)
+  | is_const_name a = (Fn a [], rest)
+  | otherwise = (Var a, rest)
+
+parse_term inp =
+  parse_right_infix
+    "++"
+    (\e1 e2 -> (Fn "++" [e1, e2]))
+    (parse_right_infix
+       "+"
+       (\e1 e2 -> (Fn "+" [e1, e2]))
+       (parse_left_infix
+          "-"
+          (\e1 e2 -> (Fn "-" [e1, e2]))
+          (parse_right_infix
+             "*"
+             (\e1 e2 -> (Fn "*" [e1, e2]))
+             (parse_left_infix
+                "/"
+                (\e1 e2 -> (Fn "/" [e1, e2]))
+                (parse_left_infix
+                   "^"
+                   (\e1 e2 -> (Fn "^" [e1, e2]))
+                   (parse_atomic_term))))))
+    inp
+
+parset = make_parser parse_term
+
+-- Appendix 3 Parsing first-order formulas
+data FOL =
+  R String
+    [Term]
+  deriving (Show, Eq)
+
+data Formula a
+  = Bottom
+  | Top
+  | Atom a
+  | Not (Formula a)
+  | And (Formula a)
+        (Formula a)
+  | Or (Formula a)
+       (Formula a)
+  | Imp (Formula a)
+        (Formula a)
+  | Iff (Formula a)
+        (Formula a)
+  | Forall String
+           (Formula a)
+  | Exists String
+           (Formula a)
+  deriving (Show, Eq)
+
+parse_atom inp =
+  let (tm, rest) = parse_term inp
+   in if rest /= [] && head rest `elem` ["=", "<", "<=", ">", ">="]
+        then papply
+               (\tm' -> Atom (R (head rest) [tm, tm']))
+               (parse_term $ tail rest)
+        else case inp of
+               (p:"(":")":rest') -> (Atom (R p []), rest')
+               (p:"(":rest') ->
+                 papply
+                   (\args -> Atom (R p args))
+                   (parse_bracketed (parse_list "," parse_term) ")" rest')
+               (p:rest') ->
+                 if p /= "("
+                   then (Atom (R p []), rest')
+                   else error "parse_atom"
+
+parse_atomic_formula afn inp =
+  case inp of
+    [] -> error "formula expected"
+    "false":rest -> (Bottom, rest)
+    "true":rest -> (Top, rest)
+    "(":rest -> parse_bracketed (parse_formula afn) ")" rest
+    "~":rest -> papply (\p -> Not p) (parse_atomic_formula afn rest)
+    "forall":x:rest -> parse_quant afn (\x p -> (Forall x p)) x rest
+    "exists":x:rest -> parse_quant afn (\x p -> (Exists x p)) x rest
+    _ -> afn inp
+
+parse_quant afn qcon x inp =
+  case inp of
+    [] -> error "Body of quantified term expected"
+    y:rest ->
+      papply
+        (\fm -> (qcon x fm))
+        (if y == "."
+           then parse_formula afn rest
+           else parse_quant afn qcon y rest)
+
+parse_formula afn inp =
+  parse_right_infix
+    "<=>"
+    (\p q -> (Iff p q))
+    (parse_right_infix
+       "==>"
+       (\p q -> (Imp p q))
+       (parse_right_infix
+          "\\/"
+          (\p q -> (Or p q))
+          (parse_right_infix
+             "/\\"
+             (\p q -> (And p q))
+             (parse_atomic_formula afn))))
+    inp
+
+parse = make_parser (parse_formula parse_atom)
---
+++ Printer_FOL.hs
@@ -0,0 +1,102 @@
+-- Printing of formulas --
+-- After Harrison "Handbook ... "
+--
+import Data.String
+import Parsing_FOL
+import Text.PrettyPrint.HughesPJClass as PP
+                        -- forgets all quantifier, we need to flag them later.
+
+----------- FOL Printer Appendix 3
+-------- bracket function without openbox is pointless...
+strip_quant fm =
+  case fm of
+    Forall x (Forall y p) ->
+      let (xs, q) = strip_quant (Forall y p)
+       in (x : xs, q)
+    Exists x (Exists y p) ->
+      let (xs, q) = strip_quant (Exists y p)
+       in (x : xs, q)
+    Forall x p -> ([x], p)
+    Exists x p -> ([x], p)
+    _ -> ([], fm) ------ print_formula mutually recursive functions ------
+
+print_qnt pfn qname (bvs, bod) =
+  hcat
+    ([PP.text qname] ++
+     map (\v -> PP.space <> PP.text v) bvs ++ [PP.text "."] ++ [PP.space]) <>
+  print_formula pfn 0 bod
+
+print_prefix pfn newpr sym p = (PP.text sym) <> print_formula pfn (newpr + 1) p
+
+print_infix pfn newpr sym p q =
+  print_formula pfn (newpr + 1) p <+>
+  text sym <> PP.space <> print_formula pfn newpr q
+
+print_formula pfn pr fm =
+  case fm of
+    Top -> PP.text "false"
+    Bottom -> PP.text "true"
+    Atom (pargs) -> pfn pr pargs
+    Not p -> PP.maybeParens (pr > 10) (print_prefix pfn 10 "~" p)
+    And p q -> PP.maybeParens (pr > 8) (print_infix pfn 8 "/\\" p q)
+    Or p q -> PP.maybeParens (pr > 6) (print_infix pfn 6 "\\/" p q)
+    Imp p q -> PP.maybeParens (pr > 4) (print_infix pfn 4 "==>" p q)
+    Iff p q -> PP.maybeParens (pr > 2) (print_infix pfn 2 "<=>" p q)
+    Forall x p ->
+      PP.maybeParens (pr > 0) (print_qnt pfn "forall" (strip_quant fm))
+    Exists x p ->
+      PP.maybeParens (pr > 0) (print_qnt pfn "exists" (strip_quant fm))
+
+print_formula1 pfn = print_formula pfn 0
+        -------- first-order term printer, used to print formulas --------
+
+print_term prec fm =
+  case fm of
+    Var x -> text x
+    Fn "^" [tm1, tm2] -> print_infix_term True prec 24 "^" tm1 tm2
+    Fn "/" [tm1, tm2] -> print_infix_term True prec 22 " /" tm1 tm2
+    Fn "*" [tm1, tm2] -> print_infix_term False prec 20 " *" tm1 tm2
+    Fn "-" [tm1, tm2] -> print_infix_term True prec 18 " -" tm1 tm2
+    Fn "+" [tm1, tm2] -> print_infix_term True prec 16 " +" tm1 tm2
+    Fn "::" [tm1, tm2] -> print_infix_term True prec 14 "::" tm1 tm2
+    Fn f args -> print_fargs f args
+
+print_fargs f args =
+  PP.text f <>
+  if args == []
+    then PP.empty
+    else (PP.text "(" <> print_term 0 (head args) <+>
+          hcat
+            (map (\t -> PP.comma <> PP.space <> print_term 0 t) (tail args) ++
+             [PP.rparen]))
+
+print_infix_term isleft oldprec newprec sym p q =
+  if oldprec > newprec
+    then (PP.lparen)
+    else PP.empty <>
+         print_term
+           (if isleft
+              then newprec
+              else newprec + 1)
+           p <>
+         PP.text sym <>
+         PP.space <>
+         print_term
+           (if isleft
+              then newprec + 1
+              else newprec)
+           q <>
+         if oldprec > newprec
+           then (PP.rparen)
+           else PP.empty
+
+printert tm = print_term 0 tm
+        --------- first-order formula printer, substituted for pfn -----
+
+print_atom prec (R p args) =
+  if elem p ["=", "<", "<=", ">="] && length args == 2
+    then print_infix_term False 12 12 (" " ++ p) (args !! 0) (args !! 1)
+    else print_fargs p args
+        ----- print_fol_formula
+
+pprint = print_formula1 print_atom
