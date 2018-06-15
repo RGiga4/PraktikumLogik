d6119a4798baed688813ddaae285327ac5b9ab91
Pi <roman.pleskovsky@gmail.com>
2018-06-15 16:23:33 +0200

    Bessere Version

--- MyVortrag.hs
+++ MyVortrag.hs
@@ -8,25 +8,27 @@ import Parsing_FOL
 import Printer_FOL
 --import Vortrag1
 
--- soll Terme mit n funktion geben
---groundterms :: [a] -> [b] -> c -> [d]
+termint a = (Fn a [])
+
+-- soll alle Terme mit n funktion geben
+
 groundterms cntms funcs n 
 	| n == 0 = cntms
-	| otherwise =  [(fst f)++" "++x | f  <- funcs, x <- groundtuples cntms funcs (n-1) (snd f)]
+	| otherwise = [(Fn (fst f) x) | f  <- funcs, x <- groundtuples cntms funcs (n-1) (snd f)]
 
--- soll Terme fur m stellen und mit n funtionen geben
+-- soll alle [Term] fur m stellen und mit n funtionen geben
 groundtuples cntms funcs n m =
 	if m == 0 then 	
-		if n == 0 then [""]
+		if n == 0 then [[]]
 		else []
-	else [x1 ++" "++ x2| k <- [0..n], x1 <- groundterms cntms funcs k, 
+	else [[x1] ++ x2 | k <- [0..n], x1 <- groundterms cntms funcs k, 
 		x2 <- groundtuples cntms funcs (n-k) (m-1) ]
 	
 
 
-t1 = groundterms ["0","1"] [("f", 1)] 1
-t2 = groundtuples ["0","1"] [("f", 1)] 0 1
-t3 = groundtuples ["0","1"] [("f", 1)] 1 1
-t4 = groundterms ["0"] [("f", 1)] 10
-t5 = groundterms ["1"] [("add", 2)] 3
-t6 = groundterms ["0"] [("add", 2), ("s", 1)] 3
+t1 = groundterms [(Fn "0" []), (Fn "1" [])] [("f", 1)] 1
+t2 = groundtuples [(Fn "0" []), (Fn "1" [])] [("f", 1)] 0 1
+t3 = groundtuples [(Fn "0" []), (Fn "1" [])] [("f", 1)] 1 1
+t4 = groundterms [(Fn "0" [])] [("f", 1)] 10
+t5 = groundterms [(Fn "1" [])] [("add", 2)] 1
+t6 = groundterms [(Fn "0" [])] [("add", 2), ("succ", 1)] 3
